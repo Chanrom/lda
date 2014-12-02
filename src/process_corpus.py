ac65b680a89b601134fe023445550a8146475ef0
Chanrom <zhouqianron@163.com>
2014-12-02 21:20:05 +0800

    modified

--- src/process_corpus.py
+++ src/process_corpus.py
@@ -26,19 +26,19 @@ all_tokens = sum(texts, [])
 tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
 texts = [[word for word in text if word not in tokens_once] for text in texts]
 
-print texts
+print 'texts:\n', texts
 
 dictionary = corpora.Dictionary(texts)
 dictionary.save('/tmp/deerwester.dict') # store the dictionary, for future reference
 
-print dictionary
+print 'dictionary:\n', dictionary
 
-print dictionary.token2id
+print 'dictionary token2id:\n', dictionary.token2id
 
 new_doc = "Human computer interaction"
 new_vec = dictionary.doc2bow(new_doc.lower().split())
-print new_vec # the word "interaction" does not appear in the dictionary and is ignored
+print 'new_vec:\n', new_vec # the word "interaction" does not appear in the dictionary and is ignored
 
 corpus = [dictionary.doc2bow(text) for text in texts]
 corpora.MmCorpus.serialize('/tmp/deerwester.mm', corpus) # store to disk, for later use
-print corpus
+print 'corpus:\n', corpus
